f5903860b2e4dcdd1ad6d192562c80a1fb57dc33
Sagar <sagarmaheshwari200761@gmail.com>
2026-08-15 16:38:42 +0530

    auth seller error

--- server/middlewares/authSeller.js
+++ server/middlewares/authSeller.js
@@ -1,18 +1,36 @@
 import jwt from 'jsonwebtoken'
-const authSeller= async (req,res,next)=>{
-    const {sellerToken}=req.cookies
-    if(!sellerToken){
-        res.json({succes: false, message: 'Not Authorized'})
+
+const authSeller = async (req, res, next) => {
+    const { sellerToken } = req.cookies
+
+    if (!sellerToken) {
+        return res.json({
+            success: false,
+            message: 'Not Authorized'
+        })
     }
-    try{
-        const tokenDecode=jwt.verify(sellerToken, process.env.JWT_SECRET)
-        if(tokenDecode.email === process.env.SELLER_EMAIL){
+
+    try {
+        const tokenDecode = jwt.verify(
+            sellerToken,
+            process.env.JWT_SECRET
+        )
+
+        if (tokenDecode.email === process.env.SELLER_EMAIL) {
             next()
         } else {
-            return res.json({success:false, message:'Not Authorized'})
+            return res.json({
+                success: false,
+                message: 'Not Authorized'
+            })
         }
-    } catch (err){
-        res.json({success:false, message:err.message})
+
+    } catch (err) {
+        return res.json({
+            success: false,
+            message: err.message
+        })
     }
 }
+
 export default authSeller
