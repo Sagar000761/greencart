57d014c3619d2e04a8b6052753d0dee055f5f614
Sagar <sagarmaheshwari200761@gmail.com>
2026-08-21 15:44:08 +0530

    loading

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,36 +1,34 @@
 import React, { useEffect } from "react";
-
 import useAppContext from "../context/AppContext";
-
 import { useLocation } from "react-router-dom";
 
 const Loading = () => {
     const { navigate, fetchUser } = useAppContext();
-
     const { search } = useLocation();
 
     const query = new URLSearchParams(search);
-
     const nextUrl = query.get("next");
 
     useEffect(() => {
         const handlePaymentSuccess = async () => {
-            // Give Stripe webhook some time to update database
-            await new Promise((resolve) => setTimeout(resolve, 2000));
+            if (!nextUrl) return;
 
-            // Fetch latest user/cart from database
+            // Give Stripe webhook a little time
+            await new Promise((resolve) =>
+                setTimeout(resolve, 2000)
+            );
+
+            // Get fresh user + cart from database
             await fetchUser();
 
-            // Small delay to make sure state is updated
-            setTimeout(() => {
-                if (nextUrl) {
-                    navigate(`/${nextUrl}`, { replace: true });
-                }
-            }, 500);
+            // Go to My Orders
+            navigate(`/${nextUrl}`, {
+                replace: true
+            });
         };
 
         handlePaymentSuccess();
-    }, [nextUrl, navigate]);
+    }, [nextUrl]);
 
     return (
         <div className="flex justify-center items-center h-screen">
