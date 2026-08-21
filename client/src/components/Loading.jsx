632a879f082d43ef0ac71d510cc2e4ac48b6347b
Sagar <sagarmaheshwari200761@gmail.com>
2026-08-21 15:31:13 +0530

    loading changes

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,29 +1,42 @@
-import React, { useEffect } from 'react';
-import useAppContext from '../context/AppContext';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect } from "react";
+
+import useAppContext from "../context/AppContext";
+
+import { useLocation } from "react-router-dom";
 
 const Loading = () => {
-  const { navigate } = useAppContext();
-  const { search } = useLocation();
+    const { navigate, fetchUser } = useAppContext();
 
-  const query = new URLSearchParams(search);
-  const nextUrl = query.get('next');
+    const { search } = useLocation();
 
-  useEffect(() => {
-    if (nextUrl) {
-      const timer = setTimeout(() => {
-        navigate(`/${nextUrl}`);
-      }, 5000);
+    const query = new URLSearchParams(search);
 
-      return () => clearTimeout(timer);
-    }
-  }, [nextUrl, navigate]);
+    const nextUrl = query.get("next");
 
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-24 w-24 border-4 border-gray-300 border-t-primary"></div>
-    </div>
-  );
+    useEffect(() => {
+        const handlePaymentSuccess = async () => {
+            // Give Stripe webhook some time to update database
+            await new Promise((resolve) => setTimeout(resolve, 2000));
+
+            // Fetch latest user/cart from database
+            await fetchUser();
+
+            // Small delay to make sure state is updated
+            setTimeout(() => {
+                if (nextUrl) {
+                    navigate(`/${nextUrl}`, { replace: true });
+                }
+            }, 500);
+        };
+
+        handlePaymentSuccess();
+    }, [nextUrl, navigate]);
+
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="animate-spin rounded-full h-24 w-24 border-4 border-gray-300 border-t-primary"></div>
+        </div>
+    );
 };
 
 export default Loading;
